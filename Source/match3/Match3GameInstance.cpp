9895f749087e3c496f11df3807b02653ee46913b
caodhuan <cao.d.huan@gmail.com>
2025-03-14 00:02:49 +0800

    misc

---
+++ Source/match3/Match3GameInstance.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Match3GameInstance.h"
+
+
+UMatch3GameInstance::UMatch3GameInstance()
+{
+	DefaultSaveGameSlot = TEXT("_Match3Game");
+
+}
---
+++ Source/match3/Match3GameInstance.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "Match3GameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MATCH3_API UMatch3GameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+public:
+	UMatch3GameInstance();
+};
